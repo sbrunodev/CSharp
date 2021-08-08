b6716e997b989d253e05da1ed4ebae993c0816a9
sbrunodev <brunosilva.dev@outlook.com>
2021-08-08 20:42:01 -0300

    Carregando excel com ExcelDataReader

---
+++ ManipulatingExcel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using System.IO;
+using ExcelDataReader;
+
+namespace ManipulatingExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                ExcelDataReader
+                ExcelDataReader.DataSet
+            */
+
+            FileStream fStream = File.Open(@"C:\temp\Dados.xlsx", FileMode.Open, FileAccess.Read);
+            IExcelDataReader excelDataReader = ExcelReaderFactory.CreateOpenXmlReader(fStream);
+            DataSet resultDataSet = excelDataReader.AsDataSet();
+            excelDataReader.Close();
+        }
+    }
+}
