99da2faade6d3b45493563a0072253f3fa5b9bce
sbrunodev <brunosilva.dev@outlook.com>
2021-08-08 21:25:17 -0300

    DataSet to DataTable

--- ManipulatingExcel/Program.cs
+++ ManipulatingExcel/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using ExcelDataReader;
 
@@ -14,9 +16,38 @@ namespace ManipulatingExcel
             */
 
             FileStream fStream = File.Open(@"C:\temp\Dados.xlsx", FileMode.Open, FileAccess.Read);
+
+
             IExcelDataReader excelDataReader = ExcelReaderFactory.CreateOpenXmlReader(fStream);
             DataSet resultDataSet = excelDataReader.AsDataSet();
             excelDataReader.Close();
+
+            DataSetToDataTable(resultDataSet);
+            Console.ReadKey();   
+        }
+
+        public static DataTable DataSetToDataTable(DataSet resultDataSet)
+        {
+            var dt = resultDataSet.Tables;
+
+            var dtPlanilhaPessoas = resultDataSet.Tables[0];
+
+            List<string> columnsList = new List<string>();
+
+            for (int i = 0; i < dtPlanilhaPessoas.Columns.Count; i++)
+                columnsList.Add(dtPlanilhaPessoas.Columns[i].ToString());
+
+            foreach (DataRow dr in dtPlanilhaPessoas.Rows)
+            {
+                foreach(string column in columnsList)
+                {
+                    Console.WriteLine(column);
+                    Console.WriteLine(dr[column].ToString());
+                }
+            }
+
+
+            return dtPlanilhaPessoas;
         }
     }
 }
