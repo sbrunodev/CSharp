02e2b93d9b97de303895655eac1470e4e3814179
sbrunodev <brunosilva.dev@outlook.com>
2021-08-12 21:58:15 -0300

    Primeiro commit DDD

---
+++ DDD/Infra.Data/Context/MySqlContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Infra.Data.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infra.Data.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(new UserMap().Configure);
+        }
+    }
+}
---
+++ DDD/Service/Validators/UserValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Please enter the name.")
+                .NotNull().WithMessage("Please enter the name.");
+
+            RuleFor(c => c.Email)
+                .NotEmpty().WithMessage("Please enter the email.")
+                .NotNull().WithMessage("Please enter the email.");
+
+            RuleFor(c => c.Password)
+                .NotEmpty().WithMessage("Please enter the password.")
+                .NotNull().WithMessage("Please enter the password.");
+        }
+    }
+}
