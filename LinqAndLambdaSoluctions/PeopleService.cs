690b007cf9428a47539a046207c4d2be85bbe03c
sbrunodev <brunosilva.dev@outlook.com>
2021-08-16 21:00:41 -0300

    Consulta com linq

---
+++ LinqAndLambdaSoluctions/PeopleService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqAndLambdaSoluctions
+{
+    public class PeopleService
+    {
+        public string[] GetPeopleNamesAccordingToYearOfBirth(int year)
+        {
+            var people = new[]
+            {
+                new {Name = "José", DataBirth = new DateTime(1982, 03, 27), Active = true},
+                new {Name = "Leandro", DataBirth = new DateTime(1978, 04, 03), Active = true},
+                new {Name = "Pedro", DataBirth = new DateTime(1980, 05, 24), Active = true},
+            };
+
+            return people
+                .Where(x => x.DataBirth.Year >= year)
+                .Select(x => x.Name).ToArray();
+                
+        }
+
+    }
+}
---
+++ LinqAndLambdaSoluctions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinqAndLambdaSoluctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new PeopleService();
+
+            var peoples = service.GetPeopleNamesAccordingToYearOfBirth(1980);
+
+            foreach (var peopleName in peoples)
+                Console.WriteLine(peopleName);
+
+            Console.ReadKey();
+
+        }
+    }
+}
